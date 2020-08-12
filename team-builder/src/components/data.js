3fe6e0fbee6ec19370d7dfe8aaa0f5174edf3a36
Hamida Sharifova <hamida@Hamidas-MacBook-Pro.local>
2020-08-11 19:10:01 -0700

    SPA-Forms-Team Builder MVP

---
+++ team-builder/src/components/data.js
@@ -0,0 +1,32 @@
+export default [
+    {
+        name: "Borat Sagdiyev",
+        location: "Kazakhstan",
+        img: "https://lambda-build1-marketing.netlify.app/design/review-img/borat-high-five.jpg",
+        quote: "Cultural Learnings of America for Make Benefit Glorious Nation of Kazakhstan"
+    },
+
+    {
+        name: "Carole Baskin",
+        location: "Florida, USA", 
+        img: "https://lambda-build1-marketing.netlify.app/design/review-img/Carole-Baskin.jpg",
+        quote: "They want to use those cats to elevate their status"
+    },
+
+    {
+        name: "Joe Exotic" ,
+        location: "Oklahoma, USA", 
+        img: "https://lambda-build1-marketing.netlify.app/design/review-img/joe-exotic.jpeg",
+        quote: "People don't come to see the tigers; they come to see me"
+
+    },
+
+    {
+        name: "Chuck Norris" ,
+        location: "California, USA", 
+        img: "https://lambda-build1-marketing.netlify.app/design/review-img/chuck-norris.jpeg",
+        quote: "Chuck Norris doesn't climb trees. He just pulls them down and walks on top of them."
+
+    },
+
+]
